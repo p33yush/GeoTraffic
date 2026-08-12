f863c5400e6dd2c8ad92a80428b8f5015ed74aed
p33yush <peeyushhanumante@gmail.com>
2026-08-12 06:10:34 +0530

    added spatial queries

--- db-worker/schema.sql
+++ db-worker/schema.sql
@@ -10,3 +10,20 @@ create table traffic_events(
 );
 
 CREATE INDEX traffic_events_location_idx ON traffic_events USING GIST (location);
+
+
+SELECT 
+    a.event_id as Event_1, 
+    b.event_id as Event_2, 
+    ST_Distance(a.location::geography, b.location::geography) as distance_in_meters
+FROM traffic_events a, traffic_events b
+WHERE a.id != b.id 
+LIMIT 5;
+
+SELECT event_id, type 
+FROM traffic_events 
+WHERE ST_DWithin(
+    location::geography, 
+    ST_SetSRID(ST_MakePoint(75.85, 22.71), 4326)::geography, 
+    2000
+);
